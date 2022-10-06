5b72c90234f59224587db9e226530580d6fe7545
Rodrigo Póvoa <up202108890@up.pt>
2022-10-06 15:43:49 +0100

    Version 3.0

---
+++ src/main/java/com/up202108890/hero/Application.java
@@ -0,0 +1,10 @@
+package com.up202108890.hero;
+
+import java.io.IOException;
+
+public class  Application {
+    public static void main(String[] args) throws IOException {
+        Game game= new Game();
+        game.run();
+    }
+}
